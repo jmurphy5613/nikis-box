5f04e580d2708726c6a10a1bf0984b53188eeb54
jmurphy5613 <jmurphy5613@gmail.com>
2024-01-24 20:23:47 -0500

    hi

---
+++ components/Time.tsx
@@ -0,0 +1,13 @@
+
+
+const Time = () => {
+    return (
+        <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="9" stroke="#85FF00" stroke-width="2" />
+            <path d="M16.5 12H12.25C12.1119 12 12 11.8881 12 11.75V8.5" stroke="#85FF00" stroke-width="2" stroke-linecap="round" />
+        </svg>
+
+    )
+}
+
+export default Time
